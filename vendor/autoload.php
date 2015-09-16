b20f189fe55fcaf94bb5464383d3f4042afce59d
Paul Hibbitts <paul@paulhibbitts.com>
2015-09-16 13:41:31 -0700

    Grav updates.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit2df88f831621a36d78cb272890490c89::getLoader();
+return ComposerAutoloaderInit4c93dab2b2bef220c57fe81be2767623::getLoader();
